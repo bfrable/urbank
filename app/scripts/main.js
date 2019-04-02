39a58b6e5b591b4034a3eff366742e0371c21406
Brett Frable <brett.frable@zenmonics.com>
2019-04-02 17:24:34 -0400

    responsive nav  and about content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,26 @@
-console.log('\'Allo \'Allo!');
+(function() {
+    const helpers = {
+        nav: () => {
+            const toggle = document.querySelector('.navigation-toggle');
+            const nav    = document.querySelector('.nav');
+            let navHidden = nav.currentStyle ? nav.currentStyle.display : getComputedStyle(nav, null).display;
 
+            toggle.addEventListener('click', () => {
+                if (navHidden === 'none') {
+                    toggle.classList.add('navigation-toggle--open')
+                    nav.style.display = 'block';
+                    navHidden = 'block';
+                } else {
+                    toggle.classList.remove('navigation-toggle--open')
+                    nav.style.display = 'none';
+                    navHidden = 'none';
+                }
+            });
+        },
+        init: () => {
+            helpers.nav();
+        }
+    };
+
+    helpers.init();
+})();
